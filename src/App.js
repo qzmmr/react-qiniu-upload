23ea744123b3860ad5d6fb8456541b1a513236b0
guoxinyuan <156060608792@163.com>
2018-07-11 18:39:18 +0800

    [更新]初始化七牛图片上传组件及demo

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import http from 'https-axios'
+import QiniuUpload from "./antd-upload/antd-upload";
+
+const ACTION_QINIU_TOKEN = 'ACTION_QINIU_TOKEN'
+
+class App extends Component {
+    state = {
+        value: '',
+        token: ''
+    }
+    onChange = (value) => {
+        this.setState({value: value})
+    }
+
+    componentDidMount() {
+        this.init()
+    }
+
+    init() {
+        const defaultHeader = {
+            'content-type': 'application/json; charset=utf-8',
+            'accept': 'application/json'
+        }
+        http.POST('url', {
+            data: {},
+            sign: '',
+            timestamp: ''
+        }, this, ACTION_QINIU_TOKEN, defaultHeader)
+    }
+
+    onRealSuccess = (res, action) => {
+        switch (action) {
+            case ACTION_QINIU_TOKEN: {
+                if (res.code === 200) {
+                    this.setState({token: res['data'].token})
+                }
+                else {
+                    this.onFailure(res.code, res.data, action)
+                }
+                break
+            }
+        }
+    }
+
+    onFailure = (code, data, action) => {
+
+    }
+
+    onError = () => {
+
+    }
+
+    render() {
+        return (
+            <div className="App" style={{background: '#000', padding: 10}}>
+                <QiniuUpload
+                    token={this.state.token}
+                    value={this.state.value}
+                    onChange={this.onChange}
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
